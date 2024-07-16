dc80ed5a166ac2fcbb8b87f8756a09c74abc8ea8
Labib Mahmud <54614860+streamsniperrty@users.noreply.github.com>
2024-07-16 20:20:56 +0000

    Login System

---
+++ firebaseauth.js
@@ -0,0 +1,84 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
+import {
+    getFirestore,
+    setDoc,
+    doc,
+} from "https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA16yaAxO3TOHviedgcYEFK8W53zCk8emA",
+    authDomain: "intellect-25f8c.firebaseapp.com",
+    projectId: "intellect-25f8c",
+    storageBucket: "intellect-25f8c.appspot.com",
+    messagingSenderId: "830660117047",
+    appId: "1:830660117047:web:a3d2d45284a70ad1c7f5b2",
+};
+
+// Initialize
+const app = initializeApp(firebaseConfig);
+
+const signUp = document.getElementById("submitRegister");
+
+signUp.addEventListener("click", (e) => {
+    e.preventDefault();
+    let emailTemp = document.getElementById("email").value;
+    let passwordTemp = document.getElementById("password").value;
+    let usernameTemp = document.getElementById("username").value;
+
+    let email = emailTemp.toString();
+    let password = passwordTemp.toString();
+    let username = usernameTemp.toString();
+
+    const auth = getAuth();
+    const db = getFirestore();
+
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(email)) {
+        console.error("Invalid email format.");
+    }
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            const userData = {
+                email: email,
+                username: username,
+            };
+            alert("Account created!");
+            const docRef = doc(db, "users", user.uid);
+            setDoc(docRef, userData)
+                .then(() => {
+                    window.location.href = "index.html";
+                })
+                .catch((e) => {
+                    console.log(e);
+                });
+        })
+        .catch((e) => {
+            console.log(e);
+        });
+});
+
+const login = document.getElementById("login");
+login.addEventListener("click", (e) => {
+    e.preventDefault();
+    const email = document.getElementById("logEmail").value;
+    const password = document.getElementById("logPassword").value;
+    const auth = getAuth();
+
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            alert("Login is successful");
+            let user = userCredential.user;
+            localStorage.setItem("loggedInUserId", user.uid);
+            window.location.href = "index.html";
+        })
+        .catch((e) => {
+            console.log(e);
+        });
+});
--- login.js
+++ login.js
@@ -1,18 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleSwitch = document.getElementById('switch');
-    const loginForm = document.getElementById('loginForm');
-    const registerForm = document.getElementById('registerForm');
-    const formTitle = document.querySelector('.login-title');
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleSwitch = document.getElementById("switch");
+    const loginForm = document.getElementById("loginForm");
+    const registerForm = document.getElementById("registerForm");
+    const formTitle = document.querySelector(".login-title");
 
-    toggleSwitch.addEventListener('change', function() {
+    toggleSwitch.addEventListener("change", function () {
         if (this.checked) {
-            loginForm.style.display = 'none';
-            registerForm.style.display = 'block';
-            formTitle.textContent = 'REGISTER FORM';
+            loginForm.style.display = "none";
+            registerForm.style.display = "block";
+            formTitle.textContent = "REGISTER FORM";
         } else {
-            loginForm.style.display = 'block';
-            registerForm.style.display = 'none';
-            formTitle.textContent = 'LOGIN FORM';
+            loginForm.style.display = "block";
+            registerForm.style.display = "none";
+            formTitle.textContent = "LOGIN FORM";
         }
     });
 });
+
+
+// If you enabled Analytics in your project, add the Firebase SDK for Google Analytics
+
+// Add Firebase products that you want to use
+// import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
+// import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js";
+
+
