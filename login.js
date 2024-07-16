2a71b960f25930cc4ffd1c8a67332222f5a93960
Labib Mahmud <54614860+streamsniperrty@users.noreply.github.com>
2024-07-16 17:39:45 +0000

    Buncha edits

---
+++ login.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleSwitch = document.getElementById('switch');
+    const loginForm = document.getElementById('loginForm');
+    const registerForm = document.getElementById('registerForm');
+    const formTitle = document.querySelector('.login-title');
+
+    toggleSwitch.addEventListener('change', function() {
+        if (this.checked) {
+            loginForm.style.display = 'none';
+            registerForm.style.display = 'block';
+            formTitle.textContent = 'REGISTER FORM';
+        } else {
+            loginForm.style.display = 'block';
+            registerForm.style.display = 'none';
+            formTitle.textContent = 'LOGIN FORM';
+        }
+    });
+});
